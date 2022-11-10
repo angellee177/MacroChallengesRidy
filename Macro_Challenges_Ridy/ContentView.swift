f776881d5a3add6baf1ca58d55c9e2c5cf35b515
Angellee <angelriapurnamasari17@gmail.com>
2022-11-10 16:50:42 +0700

    setup: project

---
+++ Macro_Challenges_Ridy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Macro_Challenges_Ridy
+//
+//  Created by Angel Ria Purnamasari on 10/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Macro_Challenges_Ridy/Macro_Challenges_RidyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Macro_Challenges_RidyApp.swift
+//  Macro_Challenges_Ridy
+//
+//  Created by Angel Ria Purnamasari on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Macro_Challenges_RidyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
