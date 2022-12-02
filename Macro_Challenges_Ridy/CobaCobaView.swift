646f22fcd8cefd3aa8baaa292083b074aeaba0bb
Angellee <angel.purnamasari@suppapp.com>
2022-12-02 14:14:59 +0700

    add: apps icon

--- Macro_Challenges_Ridy/CobaCobaView.swift
+++ Macro_Challenges_Ridy/CobaCobaView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import HealthKit
 
 struct CobaCobaView: View {
-    @StateObject var vmModel = VmForDashboard()
+    @StateObject var vmModel = VmForDashboardCoba()
     var body: some View {
         VStack(spacing:20 ){
             if let date = vmModel.lastTimeSleep{
@@ -33,11 +33,123 @@ struct CobaCobaView: View {
             
             Text("HR Analysis")
                 .onTapGesture {
-                    vmModel.fetchHRFromHealthKit()
+                    vmModel.fetchHR()
                 }
                 
         }
     }
+}
+
+class VmForDashboardCoba:ObservableObject{
+    
+    @Published var lastTimeSleep : Date?
+    
+    @Published var myHR : Double?
+    
+    init(){
+        
+    }
+    
+    func fetchLastSleep(){
+        HealthManager.shared.fetchSleep(){ item in
+            DispatchQueue.main.async {
+                self.lastTimeSleep = item.startDate
+            }
+            
+        }
+        
+    }
+    
+    func fetchHR(){
+        HealthManager.shared.fetchHR { item in
+            let heartRateUnit:HKUnit = HKUnit(from: "count/min")
+            DispatchQueue.main.async {
+                
+                self.myHR = item.quantity.doubleValue(for: heartRateUnit)
+            }
+        }
+    }
+    
+}
+
+class HealthManagerCoba{
+    static let shared = HealthManagerCoba()
+    
+    let hkStore = HKHealthStore()
+    
+    init(){
+    
+    }
+    
+    func request(){
+        //request
+        let readType : Set<HKSampleType> = [
+            HKQuantityType.init(.heartRate),
+            HKQuantityType.init(.oxygenSaturation),
+            HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
+        ]
+        
+        Task.init {
+            do{
+                try await hkStore.requestAuthorization(toShare: [], read: readType)
+            }catch{
+                print("error request",error.localizedDescription)
+            }
+        }
+    }
+    
+    func fetchSleep(onSucces:@escaping (HKCategorySample)->()){
+        
+        let endDate = Date()
+        let startDate = Calendar.current.date(byAdding: .day, value: -7, to: endDate)
+        
+        //1 bikin predicate
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
+        //2 bikin sort (optional)
+        let sortDesc = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        //3 bikin query respon
+        let query = HKSampleQuery(sampleType: HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!, predicate: predicate, limit: 30, sortDescriptors: [sortDesc]) { query, samples, error in
+            
+            if let result = samples as? [HKCategorySample]{
+                
+                for item in result{
+                    if item.value == 1{
+                        onSucces(item)
+                        break
+                    }
+                }
+                
+                
+            }
+        }
+        //4 hkstore run query
+        hkStore.execute(query)
+        
+    }
+    
+    func fetchHR(onSuccess:@escaping(HKQuantitySample)->()){
+        
+        let endDate = Date()
+        let startDate = Calendar.current.date(byAdding: .hour, value: -1, to: endDate)
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
+        
+        let sortDesc = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+        
+        let query = HKSampleQuery(sampleType: HKQuantityType.init(.heartRate), predicate: predicate, limit: 30, sortDescriptors: [sortDesc]) { query, samples, error in
+            
+            if let result = samples as? [HKQuantitySample]{
+                
+                dump(result)
+                if let item = result.first{
+                    onSuccess(item)
+                }
+                
+            }
+        }
+    }
+    
+    
 }
 
 struct CobaCobaView_Previews: PreviewProvider {
